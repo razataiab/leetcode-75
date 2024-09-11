a001fc6b7d0531de9a20fd9fb1c3b813e3a1157f
Raza Taiab <124519821+razataiab@users.noreply.github.com>
2024-09-11 14:58:04 +0000

    string, two pointer

---
+++ Is Palindrome.cpp
@@ -0,0 +1,41 @@
+class Solution 
+{
+    public:
+        bool isPalindrome(string s) 
+        {
+            // string filtered;
+            // for(char c : s)
+            // {
+            //     if (isalnum(c))
+            //     {
+            //         filtered += tolower(c);
+            //     }
+            // }
+
+            // string a = filtered;
+            // reverse(a.begin(), a.end());
+
+            // return a==filtered;
+
+
+
+            // string filtered;
+            // for(char c : s)
+            // {
+            //     if (isalnum(c))
+            //     {
+            //         filtered += tolower(c);
+            //     }
+            // }
+
+            // for(int i = 0, j = filtered.size(); i < j/2; i++)
+            // {
+            //     if(filtered[i] != filtered[j - 1 - i])
+            //     {
+            //         return false;
+            //     }
+            // }
+
+            // return true;
+        }
+};
